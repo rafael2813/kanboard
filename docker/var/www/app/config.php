0d4651b92e24ff7b3c3c3a72d8ab1826ad3282f6
rafael2813 <ricaiachi@gmail.com>
2024-01-26 20:13:55 -0300

    Update config.php

--- docker/var/www/app/config.php
+++ docker/var/www/app/config.php
@@ -2,3 +2,19 @@
 
 defined('ENABLE_URL_REWRITE') or define('ENABLE_URL_REWRITE', true);
 defined('LOG_DRIVER') or define('LOG_DRIVER', 'system');
+
+
+// Database driver: sqlite, mysql, postgres, odbc, dblib, or mssql (sqlite by default)
+define('DB_DRIVER', 'postgres');
+
+// Database username
+define('DB_USERNAME', 'ricaiachi');
+
+// Database password
+define('DB_PASSWORD', 'PIOb2kuscg6G');
+
+// Database hostname
+define('DB_HOSTNAME', 'ep-cold-fog-a58ourj0.us-east-2.aws.neon.tech/kanboard');
+
+// Database database name
+define('DB_NAME', 'kanboard');
