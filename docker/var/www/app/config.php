b61f4db07ae04c8b76095f2c9da9f65690da2f6f
rafael2813 <ricaiachi@gmail.com>
2024-01-26 22:09:25 -0300

    Update config.php

--- docker/var/www/app/config.php
+++ docker/var/www/app/config.php
@@ -19,6 +19,12 @@ define('DB_HOSTNAME', 'ep-cold-fog-a58ourj0.us-east-2.aws.neon.tech');
 // Database database name
 define('DB_NAME', 'kanboard');
 
+// Plugins directory
+define('PLUGINS_DIR', __DIR__.DIRECTORY_SEPARATOR.'plugins');
+
+// Plugins directory URL
+define('PLUGIN_API_URL', 'https://kanboard.org/plugins.json');
+
 // Enable/Disable plugin installer (Disabled by default for security reasons)
 // There is no code review or any approval process to submit a plugin.
 // This is up to the Kanboard instance owner to validate if a plugin is legit.
