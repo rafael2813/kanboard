0d9cea5b32cb452103c0337f0cbb5e92e0be7dfd
rafael2813 <ricaiachi@gmail.com>
2024-01-29 15:52:38 -0300

    Update config.php

--- docker/var/www/app/config.php
+++ docker/var/www/app/config.php
@@ -29,3 +29,5 @@ define('PLUGIN_API_URL', 'https://kanboard.org/plugins.json');
 // There is no code review or any approval process to submit a plugin.
 // This is up to the Kanboard instance owner to validate if a plugin is legit.
 define('PLUGIN_INSTALLER', true);
+defined('ENABLE_URL_REWRITE') or define('ENABLE_URL_REWRITE', true);
+defined('LOG_DRIVER') or define('LOG_DRIVER', system);
